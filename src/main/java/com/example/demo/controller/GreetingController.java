1d4ef1cf3e0472af4e3030dd0b7a9ecf62ad69c7
tmvf <tmvf@yandex.ru>
2019-03-24 12:28:54 +0400

    02

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -1,12 +1,23 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Message;
+import com.example.demo.repos.MessageRepo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
+    private final MessageRepo messageRepo;
+
+    public GreetingController(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "hw") String name,
                            Model model) {
@@ -16,7 +27,33 @@ public class GreetingController {
 
     @GetMapping
     public String main(Model model) {
-        model.addAttribute("some", "hi user");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text,
+                      @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+
         return "main";
     }
 }
--- src/main/resources/templates/main.ftl
+++ src/main/resources/templates/main.ftl
@@ -4,7 +4,27 @@
     <meta charset="UTF-8">
     <title>Title</title>
 </head>
-<body>
-<div>${some}</div>
+<body style="background: darkgray">
+<div>
+    <form method="post">
+        <input type="text" name="text" placeholder="Сообщение">
+        <input type="text" name="tag" placeholder="Тэг">
+        <button type="submit">Добавить</button>
+    </form>
+</div>
+<div>
+    <form method="post" action="filter">
+        <input type="text" name="filter" placeholder="Фильтр по тэгу">
+        <button type="submit">Найти</button>
+    </form>
+</div>
+<div>Список сообщений</div>
+<#list messages as message>
+    <div>
+        <b>${message.id}</b>
+        <span>${message.text}</span>
+        <i>${message.tag}</i>
+    </div>
+</#list>
 </body>
 </html>
